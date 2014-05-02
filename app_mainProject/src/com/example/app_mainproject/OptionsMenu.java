d65e42ef80facf99c1dae641db5ddefc98a7ac31
Axel Boldt-Christmas <xmas1915@gmail.com>
2014-05-02 17:42:48 +0200

    Fixed Color Picker

--- app_mainProject/src/com/example/app_mainproject/OptionsMenu.java
+++ app_mainProject/src/com/example/app_mainproject/OptionsMenu.java
@@ -1,7 +1,10 @@
 package com.example.app_mainproject;
 
 
+import yuku.ambilwarna.AmbilWarnaDialog;
+import yuku.ambilwarna.AmbilWarnaKotak;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -17,16 +20,31 @@ public class OptionsMenu extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_optionsmenu);
-
+		// TODO REMOVE ONLY FOR TESTING BELOW
+		DataStorage.init(getApplicationContext());
+		// TODO REMOVE ONLY FOR TESTING ABOVE
 
 
 	}
 
 	public void goToColorsMenu(View view){
 		//TODO
-		Intent intent = new Intent(OptionsMenu.this, Home.class);
-		startActivity(intent);
-
+		Integer color = DataStorage.GetInt("themeColor");
+		if (color == null) color = 0xff0000ff;
+		 AmbilWarnaDialog dialog = new AmbilWarnaDialog(this, color,
+		            new AmbilWarnaDialog.OnAmbilWarnaListener() {
+		        @Override
+		        public void onOk(AmbilWarnaDialog dialog, int color) {
+		                // color is the color selected by the user.
+		        	DataStorage.PutInt("themeColor", color);
+		        }
+		                
+		        @Override
+		        public void onCancel(AmbilWarnaDialog dialog) {
+		                // cancel was selected by the user
+		        }
+		    });
+		    dialog.show();
 	}
 
 	public void goToContactsMenu(View view){
