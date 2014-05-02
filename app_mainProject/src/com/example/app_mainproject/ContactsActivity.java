f2c74d48ae1214d66d4f88075c68c66d2236fa9d
Axel Boldt-Christmas <xmas1915@gmail.com>
2014-05-02 18:45:45 +0200

    Renamed things and cleaned up

---
+++ app_mainProject/src/com/example/app_mainproject/ContactsActivity.java
@@ -0,0 +1,237 @@
+package com.example.app_mainproject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class ContactsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_contacts);
+		addListenerButtons();
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		LoadContacts();
+	}
+
+	private void addListenerButtons() {
+		addListenerAddButton();
+		addListenerRemoveAllButton();
+		addListenerBackButton();
+		addListenerContacts();
+	}
+
+	private ContactsActivity self() {
+		return this;
+	}
+	
+	ArrayList<Integer> idList = new ArrayList<Integer>(Arrays.asList(
+			new Integer[] {
+					R.id.uid1, 
+					R.id.uid2,
+					R.id.uid3,
+					R.id.uid4,
+					R.id.uid5,
+					R.id.uid6
+			}));
+	
+	class ContactListener implements View.OnClickListener {
+		@Override
+		public void onClick(View v) {
+			int userIndex = idList.indexOf(v.getId());
+			
+			if(userIndex >= contacts.size() || userIndex < 0) {
+				return;
+			}
+			
+			Contact contact = contacts.get(userIndex);
+
+			Intent i = new Intent(self(),
+					AddEditContactActivity.class);
+			i.putExtra("ID", contact.uid);
+			startActivityForResult(i,
+					ADD_EDIT_CONTACT_ACTIVITY_REQUEST_CODE);
+		}
+	}
+	
+	ContactListener contactListener = new ContactListener();
+	
+	private void addListenerContacts() {
+		for(int i : idList) {
+			ImageView img = (ImageView) findViewById(i);
+			img.setOnClickListener(contactListener);
+		}
+	}
+
+	private void addListenerBackButton() {
+		((Button) findViewById(R.id.backButton))
+				.setOnClickListener(new View.OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						finish();
+					}
+				});
+	}
+	
+	private void addListenerAddButton() {
+		((Button) findViewById(R.id.addButton))
+				.setOnClickListener(new View.OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						Intent i = new Intent(self(),
+								AddEditContactActivity.class);
+						startActivityForResult(i,
+								ADD_EDIT_CONTACT_ACTIVITY_REQUEST_CODE);
+					}
+				});
+	}
+
+	private void addListenerRemoveAllButton() {
+		((Button) findViewById(R.id.rem))
+				.setOnClickListener(new View.OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						DataStorage.Clear();
+						ClearContacts();
+						//TableLayout tl = (TableLayout) findViewById(R.id.table);
+						//tl.removeAllViews();
+						//mapRow.clear();
+					}
+				});
+	}
+
+	//private HashMap<Integer, TableRow> mapRow = new HashMap<Integer, TableRow>();
+	
+	List<Contact> contacts;
+	
+	int uids[] = new int[6];
+	
+	private void ClearContacts() {
+		for(int i : idList) {
+			ImageView img = (ImageView) findViewById(i);
+			img.setVisibility(View.INVISIBLE);
+		}
+		((Button) findViewById(R.id.addButton)).setVisibility(View.VISIBLE);
+	}
+
+	private void LoadContacts() {
+		List<Contact> l = DataStorage.GetAllContacts();
+		Collections.sort(l);
+		contacts = l;
+		int index = 0;
+		for(Contact c : l) {
+			if(index>=6) {
+				break;
+			}
+			ImageView img = (ImageView) findViewById(idList.get(index));
+			img.setImageBitmap(c.getImage());
+			img.setVisibility(View.VISIBLE);
+			uids[index] = c.uid;
+			index++;
+		}
+		
+		if(index >= 6) {
+			((Button) findViewById(R.id.addButton)).setVisibility(View.INVISIBLE);
+		}
+		
+		for(;index<6; ++index) {
+			ImageView img = (ImageView) findViewById(idList.get(index));
+			img.setVisibility(View.INVISIBLE);
+		}
+		/*TableLayout tl = (TableLayout) findViewById(R.id.table);
+		tl.removeAllViews();
+		mapRow.clear();
+		for (Contact c : l) {
+			TableRow row = new TableRow(this);
+			AddTableRow(c, tl, row);
+		}*/
+	}
+
+	private int ADD_EDIT_CONTACT_ACTIVITY_REQUEST_CODE = 100;
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == ADD_EDIT_CONTACT_ACTIVITY_REQUEST_CODE) {
+			if (resultCode == RESULT_OK) {
+				Bundle b = data.getExtras();
+				if (b != null)
+					ReloadContact(b.getInt("ID", -1));
+			} else if (resultCode == RESULT_CANCELED) {
+				// Do nothing
+			}
+		}
+	}
+
+	private void ReloadContact(int uid) {
+		if (uid == -1)
+			return;
+		Contact c = DataStorage.GetContact(uid);
+		if (c == null)
+			return;
+		/*TableRow row = mapRow.get(Integer.valueOf(uid));
+		TableLayout tl = (TableLayout) findViewById(R.id.table);
+		if (row == null) {
+			row = new TableRow(this);
+			AddTableRow(c, tl, row);
+		} else {
+			mapRow.remove(Integer.valueOf(uid));
+			tl.removeView(row);
+			row = new TableRow(this);
+			AddTableRow(c, tl, row);
+		}*/
+	}
+
+	private void AddTableRow(final Contact c, final TableLayout tl,
+			final TableRow row) {
+		TableRow.LayoutParams lp = new TableRow.LayoutParams(
+				TableRow.LayoutParams.WRAP_CONTENT);
+		row.setLayoutParams(lp);
+		TextView tv = new TextView(this);
+		Button btn = new Button(this);
+		tv.setText(c.getName());
+		btn.setText("Remove");
+		row.addView(btn);
+		row.addView(tv);
+		tl.addView(row);
+		//mapRow.put(c.uid, row);
+		row.setOnClickListener(new View.OnClickListener() {
+			private int uid = c.uid;
+
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(self(), AddEditContactActivity.class);
+				i.putExtra("ID", uid);
+				startActivityForResult(i,
+						ADD_EDIT_CONTACT_ACTIVITY_REQUEST_CODE);
+			}
+		});
+		btn.setOnClickListener(new View.OnClickListener() {
+			private int uid = c.uid;
+
+			@Override
+			public void onClick(View v) {
+				if (DataStorage.RemoveContact(uid))
+					tl.removeView(row);
+			}
+		});
+	}
+}
---
+++ app_mainProject/src/com/example/app_mainproject/HomeActivity.java
@@ -0,0 +1,280 @@
+package com.example.app_mainproject;
+
+
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+public class HomeActivity extends Activity {
+
+	private static final int FIVE_SECONDS = 1 * 1000; // 5s * 1000 ms/s
+	private long fiveFingerDownTime = -1;
+
+	// User profiles and id:s
+	String profile1 = "";
+	String profile2 = "";
+	String profile3 = "";
+	String profile4 = "";
+	String profile5 = "";
+	String profile6 = "";
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		DataStorage.Close();
+	}
+	
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		List<Contact> cl = DataStorage.GetAllContacts();
+		Collections.sort(cl);
+		int i = 0;
+		for (Contact c : cl) {
+			if (i >= 6) break;
+			ImageView v = GetListView(i);
+			SetString(i, c.getContactId());
+			v.setImageBitmap(c.getImage());
+			v.setVisibility(View.VISIBLE);
+			i++;
+		}
+		for (;i< 6;i++)
+			GetListView(i).setVisibility(View.INVISIBLE);
+	}
+	
+	private void SetString(int i, String s) {
+		switch (i) {
+		case 0:
+			profile1 = s;
+		case 1:
+			profile2 = s;
+		case 2:
+			profile3 = s;
+		case 3:
+			profile4 = s;
+		case 4:
+			profile5 = s;
+		case 5:
+			profile6 = s;
+		}
+	}
+	
+	private ImageView GetListView(int i) {
+		switch (i) {
+		case 0:
+			return (ImageView)findViewById(R.id.uid1);
+		case 1:
+			return (ImageView)findViewById(R.id.uid2);
+		case 2:
+			return (ImageView)findViewById(R.id.uid3);
+		case 3:
+			return (ImageView)findViewById(R.id.uid4);
+		case 4:
+			return (ImageView)findViewById(R.id.uid5);
+		case 5:
+			return (ImageView)findViewById(R.id.uid6);
+		}
+		return null;
+	}
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+		// Init SQLite Storage
+		DataStorage.init(getApplicationContext());
+
+		//
+		findViewById(R.id.layout).setOnTouchListener(new OnTouchListener() {        
+
+			@Override
+			public boolean onTouch(View v, MotionEvent ev) {
+				final int action = ev.getAction();
+
+
+				switch (action & MotionEvent.ACTION_MASK) {
+				case MotionEvent.ACTION_POINTER_DOWN:
+
+					if (ev.getPointerCount() == 5) {
+						// We have five fingers touching, so start the timer
+
+						fiveFingerDownTime = System.currentTimeMillis();
+
+					}
+					break;
+
+				case MotionEvent.ACTION_POINTER_UP:
+					if (ev.getPointerCount() < 5) {
+						// Fewer than five fingers, so reset the timer
+						fiveFingerDownTime = -1;
+					}
+					break;
+				case MotionEvent.ACTION_MOVE:
+					final long now = System.currentTimeMillis();
+					if (fiveFingerDownTime != -1 && now - fiveFingerDownTime > FIVE_SECONDS) {
+						// Five fingers have been down for 5 seconds!
+						// TODO Do something
+						fiveFingerDownTime = -1;
+						System.out.println("Five seconds has passed, an eternity awaits");
+						Intent intent = new Intent(HomeActivity.this, OptionsMenuActivity.class);
+						startActivity(intent);
+						System.out.println();
+					}
+
+					break;
+				}
+
+				return true;
+			}
+		});
+
+
+
+
+
+
+	}
+	//
+	//	@Override
+	//	public boolean onCreateOptionsMenu(Menu menu) {
+	//		// Inflate the menu; this adds items to the action bar if it is present.
+	//		getMenuInflater().inflate(R.menu.home, menu);
+	//		return true;
+	//	}
+
+
+	//Called when button is pressed
+	public void callSkypeView1 (View view){
+		if(!isSkypeClientInstalled(this)){
+			goToMarket(this);
+		}
+		else{
+			Intent skypeUriIntent = new Intent("android.intent.action.VIEW");
+			skypeUriIntent.setClassName("com.skype.raider", "com.skype.raider.Main");
+
+			//Hard-coded, change to the skype username you want to call.
+			skypeUriIntent.setData(Uri.parse("skype:" + profile1 + "?call&video=true"));
+			startActivity(skypeUriIntent);
+		}
+
+	}
+
+	public void callSkypeView2 (View view){
+		if(!isSkypeClientInstalled(this)){
+			goToMarket(this);
+		}
+		else{
+			Intent skypeUriIntent = new Intent("android.intent.action.VIEW");
+			skypeUriIntent.setClassName("com.skype.raider", "com.skype.raider.Main");
+
+			//Hard-coded, change to the skype username you want to call.
+			skypeUriIntent.setData(Uri.parse("skype:" + profile2 + "?call&video=true"));
+			startActivity(skypeUriIntent);
+		}
+
+	}
+
+	public void callSkypeView3 (View view){
+		if(!isSkypeClientInstalled(this)){
+			goToMarket(this);
+		}
+		else{
+			Intent skypeUriIntent = new Intent("android.intent.action.VIEW");
+			skypeUriIntent.setClassName("com.skype.raider", "com.skype.raider.Main");
+
+			//Hard-coded, change to the skype username you want to call.
+			skypeUriIntent.setData(Uri.parse("skype:" + "joni.baitar" + "?call&video=true"));
+			startActivity(skypeUriIntent);
+		}
+
+	}
+
+	public void callSkypeView4 (View view){
+		if(!isSkypeClientInstalled(this)){
+			goToMarket(this);
+		}
+		else{
+			Intent skypeUriIntent = new Intent("android.intent.action.VIEW");
+			skypeUriIntent.setClassName("com.skype.raider", "com.skype.raider.Main");
+
+			//Hard-coded, change to the skype username you want to call.
+			skypeUriIntent.setData(Uri.parse("skype:" + "joni.baitar" + "?call&video=true"));
+			startActivity(skypeUriIntent);
+		}
+
+	}
+
+	public void callSkypeView5 (View view){
+		if(!isSkypeClientInstalled(this)){
+			goToMarket(this);
+		}
+		else{
+			Intent skypeUriIntent = new Intent("android.intent.action.VIEW");
+			skypeUriIntent.setClassName("com.skype.raider", "com.skype.raider.Main");
+
+			//Hard-coded, change to the skype username you want to call.
+			skypeUriIntent.setData(Uri.parse("skype:" + "joni.baitar" + "?call&video=true"));
+			startActivity(skypeUriIntent);
+		}
+
+	}
+
+	public void callSkypeView6 (View view){
+		if(!isSkypeClientInstalled(this)){
+			goToMarket(this);
+		}
+		else{
+			Intent skypeUriIntent = new Intent("android.intent.action.VIEW");
+			skypeUriIntent.setClassName("com.skype.raider", "com.skype.raider.Main");
+
+			//Hard-coded, change to the skype username you want to call.
+			skypeUriIntent.setData(Uri.parse("skype:" + "joni.baitar" + "?call&video=true"));
+			startActivity(skypeUriIntent);
+		}
+
+	}
+
+
+	//Install the Skype client through the market: URI scheme.
+	public void goToMarket(Context myContext) {
+		Uri marketUri = Uri.parse("market://details?id=com.skype.raider");
+		Intent myIntent = new Intent(Intent.ACTION_VIEW, marketUri);
+		myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		myContext.startActivity(myIntent);
+
+		return;
+	}
+
+	//Determine whether the Skype for Android client is installed on this device.
+	public boolean isSkypeClientInstalled(Context myContext) {
+		PackageManager myPackageMgr = myContext.getPackageManager();
+		try {
+			myPackageMgr.getPackageInfo("com.skype.raider", PackageManager.GET_ACTIVITIES);
+		}
+		catch (PackageManager.NameNotFoundException e) {
+			return (false);
+		}
+		return (true);
+	}
+
+
+
+}
+
+
+
